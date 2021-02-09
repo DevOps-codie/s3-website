1c77a4c729b61dea6b658f48239ea01383b787de
Codie Dasine <38220387+DevOps-codie@users.noreply.github.com>
2021-02-08 22:10:22 -0600

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -2,3 +2,9 @@ variable "env" {
   default = ""
   description = "environment to build"
 }
+
+variable redirect_to {
+  type        = string
+  default     = ""
+  description = "Where to redirect requests to, if not set the cloudfront distribution host will be used"
+}
