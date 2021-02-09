07e74badd1c81358817cae403e35d5f9fe295420
Codie Dasine <38220387+DevOps-codie@users.noreply.github.com>
2021-02-09 15:09:58 -0600

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -8,3 +8,16 @@ variable redirect_to {
   default     = ""
   description = "Where to redirect requests to, if not set the cloudfront distribution host will be used"
 }
+
+variable domain_name {
+  type        = string
+  default     = ""
+  description = "What is the name of the domain you would like to use ex bar.foo.com"
+}
+
+variable hosted_zone {
+  type        = string
+  default     = ""
+  description = "the hosted zone you will use ex foo.com"
+}
+
